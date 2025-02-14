0d10088639f39369e537040c845650bb961070bd
tunv <nguyenvantu191298@gmail.com>
2025-02-14 14:27:18 +0700

    feat(TSF-04): create app header for app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "@/providers/react-query-provider";
+import AppHeader from "@/components/app-header/app-header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <ReactQueryProvider>
+          <AppHeader />
+          <main className="relative">{children}</main>
+        </ReactQueryProvider>
       </body>
     </html>
   );
