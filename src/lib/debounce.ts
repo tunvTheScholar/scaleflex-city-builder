65e7840dab630915049a455ca83c00394515a82c
tunv <nguyenvantu191298@gmail.com>
2025-02-14 21:56:52 +0700

    feat(TSF-04): add debounce fn

---
+++ src/lib/debounce.ts
@@ -0,0 +1,11 @@
+export const debounce = <T extends (...args: any[]) => void>(
+  func: T,
+  delay: number
+) => {
+  let timer: NodeJS.Timeout;
+
+  return (...args: Parameters<T>) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => func(...args), delay);
+  };
+};
