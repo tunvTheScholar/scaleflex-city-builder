ee2c272f482a8384c2d15838c7ba9462c003adf5
tunv <nguyenvantu191298@gmail.com>
2025-02-14 21:59:55 +0700

    fix: update rule eslint cause we need any type

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-empty-object-type": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
   },
 ];
